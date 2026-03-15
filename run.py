5094c9aa5caa9090c2e6da77bea6b32c466df793
gurpartapxen <simmakgurpartap@gmail.com>
2026-03-15 13:11:47 +0530

    Fix DB crash on startup

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 import os
 
-# Auto-fix Railway MySQL URL format
 db_url = os.environ.get('DATABASE_URL', '')
 if db_url.startswith('mysql://'):
     os.environ['DATABASE_URL'] = db_url.replace('mysql://', 'mysql+pymysql://', 1)
 
 from app import create_app, db
+
 app = create_app()
 
-with app.app_context():
-    db.create_all()
+try:
+    with app.app_context():
+        db.create_all()
+        print('DB ready!')
+except Exception as e:
+    print(f'DB warning: {e}')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
